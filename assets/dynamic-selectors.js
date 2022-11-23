a5203ba42de7f17c2e40399cd6af3f3ec57dac65
Jono Hall <jonohall@gmail.com>
2022-11-23 13:52:48 +1300

    Refactoring

--- assets/dynamic-selectors.js
+++ assets/dynamic-selectors.js
@@ -6,14 +6,12 @@ this.rebuildOptions();
 
 //gather a list of valid combinations of options, check to see if the input passed to it matches in a chain of valid options.
 function validCombo(inputValue,optionLevel) {
-  //build an array of currently selected options
   const selectedOptions = fieldsets.map((fieldset) => {
-      var options = (pickerType == 'radios') ? Array.from(fieldset.querySelectorAll('input')).find((radio) => radio.checked).value : Array.from(fieldset.querySelectorAll('select'), (select) => select.value);
-      return options;
+      return (pickerType == 'radios') ? Array.from(fieldset.querySelectorAll('input')).find((radio) => radio.checked).value : Array.from(fieldset.querySelectorAll('select'), (select) => select.value);
   });
 
   productJson = JSON.parse(variantSelects.querySelector('[type="application/json"]').textContent);
-  for (var i = 0, validCombo = false; i < productJson.length && !validCombo; i++) {
+  for(var i = 0, validCombo = false; i < productJson.length && !validCombo; i++) {
     if(optionLevel == 1){
       validCombo = (productJson[i].option1 == selectedOptions[0] && productJson[i].option2 == inputValue) ? true : false;
     } else {
@@ -27,13 +25,13 @@ function rebuildOptions() {
   variantSelects.addEventListener('change', rebuildOptions);
   
   //loop through the option sets starting from the 2nd set and disable any invalid options
-  for (var optionLevel = 1, n = fieldsets.length; optionLevel < n; optionLevel++) {
+  for(var optionLevel = 1, n = fieldsets.length; optionLevel < n; optionLevel++) {
     const inputs = (pickerType == 'radios') ? fieldsets[optionLevel].querySelectorAll('input') : fieldsets[optionLevel].querySelectorAll('option');
 
     inputs.forEach(input => {
       input.disabled = (validCombo(input.value,optionLevel)) ? false : true;
       if(pickerType == 'radios'){
-        //get the label for the current input and hide it if it is not a valid combo option
+        //get the label for the current input (this is what the user clicks, the "pill")
         const label = fieldsets[optionLevel].querySelector(`label[for="${input.id}"]`);
 
         label.style.display = (input.disabled) ? "none" : ""; //Hide the option, or comment this line out and use the following lines to style it..
@@ -50,20 +48,14 @@ function rebuildOptions() {
   for (var optionLevel = 1, n = fieldsets.length, change = false; optionLevel < n && !change; optionLevel++) {
     if(pickerType == 'radios'){
       if(fieldsets[optionLevel].querySelector('input:checked').disabled === true) {
-        fieldsets[optionLevel].querySelector('input:not(:disabled)').checked = true;
-
-        //if an option has been changed, break out of the loop and restart the whole process with the newly selected option
-        change = true;
-        variantSelects.dispatchEvent(new Event('change', { bubbles: true }));
+        change = (fieldsets[optionLevel].querySelector('input:not(:disabled)').checked = true);
       }
     } else {
       if(fieldsets[optionLevel].querySelector('option:checked').disabled === true) {
-        fieldsets[optionLevel].querySelector('option:not(:disabled)').selected = "selected";
-        
-        //if an option has been changed, break out of the loop and restart the whole process with the newly selected option
-        change = true;
-        variantSelects.dispatchEvent(new Event('change', { bubbles: true }));
+        change = (fieldsets[optionLevel].querySelector('option:not(:disabled)').selected = "selected");
       }
     }
+    //if a new option has been selected, restart the whole process
+    if(change) variantSelects.dispatchEvent(new Event('change', { bubbles: true }));
   }
 }
